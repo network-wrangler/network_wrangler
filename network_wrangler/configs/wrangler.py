3d36f1f3bbb3968113d16fa5d584eadb22f6d3f2
Sijia Wang <wangsijia0628@gmail.com>
2025-07-19 12:31:32 -0400

    use `default_factory`

--- network_wrangler/configs/wrangler.py
+++ network_wrangler/configs/wrangler.py
@@ -218,10 +218,10 @@ class WranglerConfig(ConfigItem):
         EDITS: Parameters governing how edits are handled.
     """
 
-    IDS: IdGenerationConfig = IdGenerationConfig()
-    MODEL_ROADWAY: ModelRoadwayConfig = ModelRoadwayConfig()
-    CPU: CpuConfig = CpuConfig()
-    EDITS: EditsConfig = EditsConfig()
+    IDS: IdGenerationConfig = Field(default_factory=IdGenerationConfig())
+    MODEL_ROADWAY: ModelRoadwayConfig = Field(default_factory=ModelRoadwayConfig())
+    CPU: CpuConfig = Field(default_factory=CpuConfig())
+    EDITS: EditsConfig = Field(default_factory=EditsConfig())
 
 
 DefaultConfig = WranglerConfig()
