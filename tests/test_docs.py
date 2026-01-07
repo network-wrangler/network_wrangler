e7e5de6a85f2e658e917b31acd886237e1a5af40
Elizabeth Sall <easall@gmail.com>
2026-01-06 16:29:00 -0800

    removed test_docs.py since it isn't needed

--- tests/test_docs.py
+++
@@ -1,4 +0,0 @@
-"""Tests the documentation can be built without errors."""
-
-# Note: mkdocs build test removed - documentation builds are tested
-# in CI workflow separately via 'mike deploy' command
